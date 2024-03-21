0a59623041de0a8dbe81f168108b3ced3655dad1
asma-ad <asmaaderghalmoocs@gmail.com>
2024-03-21 17:59:29 +0000

    p10

---
+++ C++ level 6/10P_days_from_the_beginning_of_year.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+/*
+Problem #10
+write a program to print total days from the beginning of a year.
+*/
+
+short readYear()
+{
+    short number = 0;
+    cout << "Enter a year: ";
+    cin >> number;
+    return number;
+}
+short readMonth()
+{
+    short number = 0;
+    cout << "Enter a month: ";
+    cin >> number;
+    return number;
+}
+short readDay()
+{
+    short number = 0;
+    cout << "Enter a day: ";
+    cin >> number;
+    return number;
+}
+bool isLeapYear(short year)
+{
+    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+}
+short numberOfDaysInAMonth(short year, short month)
+{
+
+    if (month <= 0 || month > 12)
+        return 0;
+
+    int numberOfDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    return (month == 2) ? (isLeapYear(year) ? 29 : 28) : numberOfDays[month - 1];
+}
+
+short numberOfDaysFromTheBeginningOfTheYear(short day, short month, short year)
+{
+    short total = 0;
+    for (short i = 1; i < month; i++)
+    {
+        if (month == 1)
+            return day;
+        else
+            total += numberOfDaysInAMonth(year, i);
+    }
+    return total + day;
+}
+
+int main()
+{
+
+    short year = readYear();
+    short month = readMonth();
+    short day = readDay();
+
+    cout << "\nNumber of days from the beginning of the year is: "
+         << numberOfDaysFromTheBeginningOfTheYear(day, month, year);
+
+    return 0;
+}
